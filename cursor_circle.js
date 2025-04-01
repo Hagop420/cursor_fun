6e10d978677eb662a63b75ba966540dfce4515f9
Hagop Almadjian <hagop1isaboss@hotmail.com>
2025-03-31 22:04:49 -0700

    Done

--- cursor_circle.js
+++ cursor_circle.js
@@ -4,35 +4,22 @@ const backgroundTxt = document.querySelector('.back')
 
 document.addEventListener("DOMContentLoaded", () => {
    if (!cursor) return;
-
-   document.addEventListener("mousemove", (e) => {
+      document.addEventListener("mousemove", (e) => {
       cursor.style.display = "flex";
       document.body.style.cursor = "none";
       cursor.style.left = `${e.pageX}px`;
       cursor.style.top = `${e.pageY}px`;
    });
 });
-
-
-
-
 backgroundTxt.addEventListener('mousemove', () => {
    cursor.style.background = '#fff'
    cursor.style.border = '#000 solid 3px'
    cursor.style.opacity = '.8'
-xRayEffect.classList.add('text_xray')
-   
+   xRayEffect.classList.add('text_xray')  
 })
 backgroundTxt.addEventListener('mouseout', () => {
    cursor.style.border = 'none'
    cursor.style.opacity = '.8'
    cursor.style.background = '#000'
-xRayEffect.classList.add('text_xray')
-   
+   xRayEffect.classList.add('text_xray')  
 })
-
-
-
-
-
- 
